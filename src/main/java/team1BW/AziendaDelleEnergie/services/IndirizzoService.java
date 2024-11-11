14a3a4eb1f26c50dc3bc09734c5212a716596dd4
campion-luca <campionluca3@gmail.com>
2024-11-11 16:08:20 +0100

    aggiunto il mapping conseguente al CRUD per modificare, vedere ed eliminare gli indirizzi + bad request exception

--- src/main/java/team1BW/AziendaDelleEnergie/services/IndirizzoService.java
+++ src/main/java/team1BW/AziendaDelleEnergie/services/IndirizzoService.java
@@ -1,7 +1,6 @@
 package team1BW.AziendaDelleEnergie.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
