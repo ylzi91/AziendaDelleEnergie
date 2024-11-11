67eecaf7dec3d8258a66171b81a708d22676af54
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-11-11 11:32:15 +0100

    Initial commit

---
+++ src/main/java/team1BW/AziendaDelleEnergie/AziendaDelleEnergieApplication.java
@@ -0,0 +1,13 @@
+package team1BW.AziendaDelleEnergie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AziendaDelleEnergieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AziendaDelleEnergieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team1BW/AziendaDelleEnergie/AziendaDelleEnergieApplicationTests.java
@@ -0,0 +1,13 @@
+package team1BW.AziendaDelleEnergie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AziendaDelleEnergieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
