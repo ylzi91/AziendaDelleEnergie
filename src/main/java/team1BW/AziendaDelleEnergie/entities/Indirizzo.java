16e4a0465a1ce5e7062f6536a91ca8514eb50f3b
campion-luca <campionluca3@gmail.com>
2024-11-11 12:25:57 +0100

    aggiunto Entities "Indirizzo"

---
+++ src/main/java/team1BW/AziendaDelleEnergie/entities/Indirizzo.java
@@ -0,0 +1,39 @@
+package team1BW.AziendaDelleEnergie.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="indirizzi")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Indirizzo {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String via;
+    private int civico;
+    private String localita;
+    private int cap;
+    // many Indirizzo - one Comune
+
+
+
+    // costruttore personalizzato
+    public Indirizzo(int cap, int civico, String localita, String via) {
+        this.cap = cap;
+        this.civico = civico;
+        this.localita = localita;
+        this.via = via;
+    }
+
+}
